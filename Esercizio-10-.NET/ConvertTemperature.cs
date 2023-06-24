380e54afa182ac8c9e91a187a666779e5e70e062
emifa <emifa@LAPTOP-1TM449QB>
2023-06-24 13:25:15 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-10-.NET/ConvertTemperature.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Esercizio_10_.NET
+{
+    public class ConvertTemperature
+    {
+        public double Temperature { get; set;}
+
+        public double ConvertToFahrenheit(double tempeCelsius)
+        {
+            double converter = (tempeCelsius * 1.8) + 32;
+            return converter;
+        }
+
+        public double ConvertToCelsius(double tempeFaharenheit)
+        {
+            double convert = (tempeFaharenheit - 32) * 0.5556;
+            return convert; 
+        }
+    }
+}
---
+++ Esercizio-10-.NET/Program.cs
@@ -0,0 +1,25 @@
+﻿using Esercizio_10_.NET;
+Console.WriteLine("Please insert the number ");
+string number = Console.ReadLine();
+double num = int.Parse(number);
+
+Console.WriteLine("Do you want convert in Celsius(1) or Fahrenheit(2) ? Insert 1 or 2 ");
+string input = Console.ReadLine(); 
+
+switch(input)
+{
+    case "1":
+        ConvertTemperature ct = new ConvertTemperature();
+        Console.WriteLine(ct.ConvertToCelsius(num));
+        break;
+    
+    case "2":
+        ConvertTemperature ct2 = new ConvertTemperature();
+        Console.WriteLine(ct2.ConvertToFahrenheit(num));
+        break;
+
+    default:
+        Console.WriteLine("Insert no valid");
+        break; 
+
+}
